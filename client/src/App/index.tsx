ed89625ec73ab11dd0c8c4d099f26ca4887c375b
Boris Okhota <95587232+borissska@users.noreply.github.com>
2025-03-10 15:36:29 +0500

    lazy loading added

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import styles from "./App.module.scss";
-import MainLayout from '../MainLayout';
-import Tasks from '../pages/Tasks';
-import OrderPage from '../pages/OrdersPage';
-import EquipmentPage from '../pages/EquipmentPage';
+import MainLayout from "../MainLayout";
+
+const OrderPage = React.lazy(() => import(/* webpackChunkName: "Orders" */ "../pages/OrdersPage"));
+const EquipmentPage = React.lazy(
+  () => import(/* webpackChunkName: "Equipment" */ "../pages/EquipmentPage")
+);
+const Tasks = React.lazy(() => import(/* webpackChunkName: "Tasks" */ "../pages/Tasks"));
 
 const App: React.FC = () => {
   return (
     <div className={styles.body}>
       <Routes>
-        <Route path='/' element={ <MainLayout /> }>
-          <Route path='orders' element={ <OrderPage /> }/>
-          <Route path='equipment' element={ <EquipmentPage /> } />
-          <Route path='tasks' element={ <Tasks /> } />
+        <Route path='/' element={<MainLayout />}>
+          <Route
+            path='orders'
+            element={
+              <React.Suspense fallback={<>Идет загрузка!</>}>
+                <OrderPage />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path='equipment'
+            element={
+              <React.Suspense fallback={<>Идет загрузка!</>}>
+                <EquipmentPage />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path='tasks'
+            element={
+              <React.Suspense fallback={<>Идет загрузка!</>}>
+                <Tasks />
+              </React.Suspense>
+            }
+          />
         </Route>
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
